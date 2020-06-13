3b8c381bec42cff5b386c2537c4a52f1d82c6331
Sandipan Das <sandipan@linux.ibm.com>
2020-06-13 21:07:50 +0530

    arch-power: Add doubleword multiply-add instructions
    This introduces 128-bit addition helpers and adds the
    following instructions.
      * Multiply-Add Low Doubleword (maddld)
      * Multiply-Add High Doubleword (maddhd)
      * Multiply-Add High Doubleword Unsigned (maddhdu)
    
    Change-Id: I04e6ea5fb4978b341a6e648424de2930ad41f449
    Signed-off-by: Sandipan Das <sandipan@linux.ibm.com>

--- src/arch/power/insts/integer.cc
+++ src/arch/power/insts/integer.cc
@@ -120,6 +120,7 @@ IntArithOp::generateDisassembly(
 {
     stringstream ss;
     bool printSecondSrc = true;
+    bool printThirdSrc = false;
 
     // Generate the correct mnemonic
     string myMnemonic(mnemonic);
@@ -131,6 +132,10 @@ IntArithOp::generateDisassembly(
         !myMnemonic.compare("subfze") ||
         !myMnemonic.compare("neg")){
         printSecondSrc = false;
+    } else if (!myMnemonic.compare("maddhd") ||
+               !myMnemonic.compare("maddhdu") ||
+               !myMnemonic.compare("maddld")) {
+        printThirdSrc = true;
     }
 
     // Additional characters depending on isa bits being set
@@ -154,6 +159,12 @@ IntArithOp::generateDisassembly(
         if (_numSrcRegs > 1 && printSecondSrc) {
             ss << ", ";
             printReg(ss, _srcRegIdx[1]);
+
+            // Print the third source register
+            if (_numSrcRegs > 2 && printThirdSrc) {
+                ss << ", ";
+                printReg(ss, _srcRegIdx[2]);
+            }
         }
     }
 
